21894ca75e38fbb1a65e2ebefc9d3657de44b618
erkman77 <116757712+erkman77@users.noreply.github.com>
2024-06-26 22:04:38 +0200

    Update autoupdate-fix.sh

--- fix/autoupdate-fix.sh
+++ fix/autoupdate-fix.sh
@@ -9,6 +9,7 @@ sed "s#$old#$new#g" /usr/bin/icam_autoupdate > /usr/bin/icam-update
 
 mv /usr/bin/icam-update /usr/bin/icam_autoupdate
 chmod +x /usr/bin/icam_autoupdate
+icam_autoupdate
 fi
 
 echo "done"
