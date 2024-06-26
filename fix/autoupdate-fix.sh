fd72839b7dd204c34c582df24c0d17d0dd205a7b
erkman77 <116757712+erkman77@users.noreply.github.com>
2024-06-26 21:22:30 +0200

    Create autoupdate-fix.sh

---
+++ fix/autoupdate-fix.sh
@@ -0,0 +1,14 @@
+#!/bin/bash +x
+
+old="oscam/install/sender"
+new="ot_tune/settings"
+
+if cat /usr/bin/icam_autoupdate | grep $old
+then
+sed "s#$old#$new#g" /usr/bin/icam_autoupdate > /usr/bin/icam-update
+
+mv /usr/bin/icam-update /usr/bin/icam_autoupdate
+chmod +x /usr/bin/icam_autoupdate
+fi
+
+echo "done"
