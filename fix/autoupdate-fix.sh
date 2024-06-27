a54cbc7039a7f42e86e725d798cb747096b5cbc8
erkman77 <116757712+erkman77@users.noreply.github.com>
2024-06-27 08:24:30 +0200

    Update autoupdate-fix.sh

--- fix/autoupdate-fix.sh
+++ fix/autoupdate-fix.sh
@@ -5,11 +5,17 @@ new="ot_tune/settings"
 
 if cat /usr/bin/icam_autoupdate | grep $old
 then
-sed "s#$old#$new#g" /usr/bin/icam_autoupdate > /usr/bin/icam-update
+echo "Fixing autoupdate..."
+sed -i "s#$old#$new#g" /usr/bin/icam_autoupdate 
 
-mv /usr/bin/icam-update /usr/bin/icam_autoupdate
-chmod +x /usr/bin/icam_autoupdate
+echo -e "\e[32m all done ...\e[0m"
+  echo -e "\e[33m ... doing restart \e[0m"
+  sleep 3
+  reboot
+
+else  
+
+echo "Nothing to do, autoupdate seems to be ok"
 
 fi
 
-echo "done"
