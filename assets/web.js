5bd6d82f49cfd2facf5f389dfa3f9d2396e019b7
erkman77 <116757712+erkman77@users.noreply.github.com>
2024-07-07 18:13:52 +0200

    set

---
+++ assets/web.js
@@ -0,0 +1,42 @@
+
+
+    HTML CSS JS 
+
+    Result
+    Skip Results Iframe
+
+EDIT ON
+
+const copyButtonLabel = "Copy Code";
+
+// use a class selector if available
+let blocks = document.querySelectorAll("pre");
+
+blocks.forEach((block) => {
+  // only add button if browser supports Clipboard API
+  if (navigator.clipboard) {
+    let button = document.createElement("button");
+
+    button.innerText = copyButtonLabel;
+    block.appendChild(button);
+
+    button.addEventListener("click", async () => {
+      await copyCode(block, button);
+    });
+  }
+});
+
+async function copyCode(block, button) {
+  let code = block.querySelector("code");
+  let text = code.innerText;
+
+  await navigator.clipboard.writeText(text);
+
+  // visual feedback that task is completed
+  button.innerText = "Code Copied";
+
+  setTimeout(() => {
+    button.innerText = copyButtonLabel;
+  }, 700);
+}
+
